e1218e00a362782eaa75a0246880d3d1cf417f95
Trần Gia Bảo <trangiabao02032003@gmail.com>
2025-04-12 21:07:09 +0700

    finish project

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,10 +1,13 @@
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import { Loader } from "./";
+import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
+import { Loader } from ".";
 
-const commonStyles =
+const companyCommonStyles =
 	"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Input = ({ placeholder, name, type, value, handleChange }) => (
@@ -19,9 +22,24 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-	const connectWallet = () => {};
+	const {
+		currentAccount,
+		connectWallet,
+		handleChange,
+		sendTransaction,
+		formData,
+		isLoading,
+	} = useContext(TransactionContext);
 
-	const handleSubmit = () => {};
+	const handleSubmit = (e) => {
+		const { addressTo, amount, keyword, message } = formData;
+
+		e.preventDefault();
+
+		if (!addressTo || !amount || !keyword || !message) return;
+
+		sendTransaction();
+	};
 
 	return (
 		<div className="flex w-full justify-center items-center">
@@ -34,21 +52,33 @@ const Welcome = () => {
 						Explore the crypto world. Buy and sell cryptocurrencies easily on
 						Krypto.
 					</p>
-					<button
-						type="button"
-						onClick={connectWallet}
-						className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-						<AiFillPlayCircle className="text-white mr-2" />
-						<p className="text-white text-base font-semibold">Connect Wallet</p>
-					</button>
+					{!currentAccount && (
+						<button
+							type="button"
+							onClick={connectWallet}
+							className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+							<AiFillPlayCircle className="text-white mr-2" />
+							<p className="text-white text-base font-semibold">
+								Connect Wallet
+							</p>
+						</button>
+					)}
 
 					<div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
-						<div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
-						<div className={commonStyles}>Security</div>
-						<div className={`sm:rounded-tr-2xl ${commonStyles}`}>Ethereum</div>
-						<div className={`sm:rounded-bl-2xl ${commonStyles}`}>Web 3.0</div>
-						<div className={commonStyles}>Low Fees</div>
-						<div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
+						<div className={`rounded-tl-2xl ${companyCommonStyles}`}>
+							Reliability
+						</div>
+						<div className={companyCommonStyles}>Security</div>
+						<div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
+							Ethereum
+						</div>
+						<div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
+							Web 3.0
+						</div>
+						<div className={companyCommonStyles}>Low Fees</div>
+						<div className={`rounded-br-2xl ${companyCommonStyles}`}>
+							Blockchain
+						</div>
 					</div>
 				</div>
 
@@ -62,7 +92,9 @@ const Welcome = () => {
 								<BsInfoCircle fontSize={17} color="#fff" />
 							</div>
 							<div>
-								<p className="text-white font-light text-sm">Address</p>
+								<p className="text-white font-light text-sm">
+									{shortenAddress(currentAccount)}
+								</p>
 								<p className="text-white font-semibold text-lg mt-1">
 									Ethereum
 								</p>
@@ -74,30 +106,30 @@ const Welcome = () => {
 							placeholder="Address To"
 							name="addressTo"
 							type="text"
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder="Amount (ETH)"
 							name="amount"
 							type="number"
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder="Keyword (Gif)"
 							name="keyword"
 							type="text"
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 						<Input
 							placeholder="Enter Message"
 							name="message"
 							type="text"
-							handleChange={() => {}}
+							handleChange={handleChange}
 						/>
 
 						<div className="h-[1px] w-full bg-gray-400 my-2" />
 
-						{false ? (
+						{isLoading ? (
 							<Loader />
 						) : (
 							<button
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	mode: "jit",
 	darkMode: false, // or 'media' or 'class'
@@ -31,5 +31,5 @@ module.exports = {
 	variants: {
 		extend: {},
 	},
-	plugins: [require("@tailwindcss/forms")],
+	plugins: [import("@tailwindcss/forms")], // Use dynamic import
 };
